e446666e31bf770fd9d135c3fee6fec2f99dbbda
LucinaLP <lucina.laurens103@gmail.com>
2022-06-25 11:42:35 -0300

    creando modelo Nosotros y Servicios

--- ProyectoBazar/ProyectoBazarApp/models.py
+++ ProyectoBazar/ProyectoBazarApp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from phonenumbers import PhoneNumber
+
 
 class Articulo(models.Model):      
     producto = models.CharField("Artículo", max_length=30)
@@ -6,4 +8,13 @@ class Articulo(models.Model):
     cantidad = models.PositiveIntegerField("Cantidad")
     color = models.CharField("Color", max_length=15)
     precio = models.FloatField("Precio $")
+
+class Nosotros(models.Model):
+    direccion = models.CharField("Dirección", max_length=30)
+    telefono = PhoneNumber("Teléfono")
+    email = models.EmailField("Email")
+    instagram = models.CharField("Instagram", max_length=30)
     
+class Servicios(models.Model):
+    nombre = models.CharField("Servicio",max_length=30)
+        

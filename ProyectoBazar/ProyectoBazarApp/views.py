91a77f808ae19b101da1f5836bc6fe368e9aac1e
LucinaLP <lucina.laurens103@gmail.com>
2022-06-25 11:48:48 -0300

    creando vistas nosotros y servicios

--- ProyectoBazar/ProyectoBazarApp/views.py
+++ ProyectoBazar/ProyectoBazarApp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from .models import Articulo
+from .models import *
 
 
 def index(request):
-    articulos=Articulo.objects.all()
-    contexto={'articulos': articulos}
+    articulos = Articulo.objects.all()
+    contexto = {'articulos': articulos}
+    return render(request,"ProyectoBazarApp/index.html")
+
+def nosotros(request):
+    info = Nosotros.objects.all()
+    contexto = {'info': info}
+    return render(request,"ProyectoBazarApp/index.html")
+
+def servicios(request):
+    servicio = Servicios.objects.all()
+    contexto = {'servicios': servicios}
     return render(request,"ProyectoBazarApp/index.html")

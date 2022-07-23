aea3e899ae34285a5535f8c6aa929f12fb9fbc75
LucinaLP <lucina.laurens103@gmail.com>
2022-07-23 10:51:09 -0300

    agregando editar perfil

--- ProyectoBazar/ProyectoBazarApp/forms.py
+++ ProyectoBazar/ProyectoBazarApp/forms.py
@@ -12,11 +12,28 @@ class NuevoArticulo(forms.Form):
     
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(label="Email")
-    password1: forms.CharField(label="Contraseña", widget=forms.PasswordInput)
-    password2: forms.CharField(label="Confirmar contraseña", widget=forms.PasswordInput)
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirmar contraseña", widget=forms.PasswordInput)
+    
+    first_name = forms.CharField(label="Nombre", required=False)
+    last_name = forms.CharField(label="Apellido", required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
         
         help_texts = {k:"" for k in fields}
+        
+class UserEditForm(UserCreationForm):
+    email = forms.EmailField(label="Email")
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput, required=False)
+    password2 = forms.CharField(label="Confirmar contraseña", widget=forms.PasswordInput, required=False)
+    
+    first_name = forms.CharField(label="Nombre")
+    last_name = forms.CharField(label="Apellido")
+
+    class Meta:
+        model = User
+        fields = ['email', 'password1', 'password2']
+        
+        #help_texts = {k:"" for k in fields}
--- ProyectoBazar/ProyectoBazarApp/urls.py
+++ ProyectoBazar/ProyectoBazarApp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login', login_request, name="login"),
     path('register', register_request, name="register"),
     path('logout', logout_request, name="logout"),
+    path('editar_perfil', editar_perfil, name="editar_perfil"),
     
     path('nosotros/',nosotros,name="nosotros"),
     path('servicios/',servicios,name="servicios"),
